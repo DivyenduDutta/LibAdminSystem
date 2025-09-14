72833f649c48439a34fd25e618521b13f6f0bb0b
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-14 19:44:33 +0200

    [FEAT] Add database context
    Added the context class for EF Core to interact with MySQL DB

---
+++ Context/LibraryContext.cs
@@ -0,0 +1,18 @@
+﻿using LibAdminSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibAdminSystem.Context
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<Book> Books => Set<Book>();
+        public DbSet<Member> Members => Set<Member>();
+        public DbSet<Loan> Loans => Set<Loan>();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "server=localhost;database=LibraryDb;user=root;password=divyendu";
+            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+        }
+    }
+}
