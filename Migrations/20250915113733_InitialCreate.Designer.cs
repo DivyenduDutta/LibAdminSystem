9af22a79bcf83a1cf4de0b3880aca95fef5f3d99
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-15 13:45:46 +0200

    [CHORE] Update EF Core migrations

--- Migrations/20250915113733_InitialCreate.Designer.cs
+++ Migrations/20250915113733_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibAdminSystem.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    [Migration("20250915074414_InitialCreate")]
+    [Migration("20250915113733_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
