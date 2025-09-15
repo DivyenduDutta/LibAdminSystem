a614dc2e22a5832c6b440dfb9fb811a1093edf1d
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-15 13:48:11 +0200

    [FEAT] Add a design-time factory
    This ensures that when the library context is created it doesnt use the Program.cs DI

---
+++ Context/LibraryContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace LibAdminSystem.Context
+{
+
+    public class LibraryContextFactory : IDesignTimeDbContextFactory<LibraryContext>
+    {
+        public LibraryContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<LibraryContext>();
+            optionsBuilder.UseMySql(
+                config.GetConnectionString("LibAdminSystem"),
+                ServerVersion.AutoDetect(config.GetConnectionString("LibAdminSystem"))
+            );
+
+            return new LibraryContext(optionsBuilder.Options);
+        }
+    }
+
+}
