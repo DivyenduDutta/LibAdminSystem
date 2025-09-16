b07f4170fb76566acd60b79726ae3e205c99f3fe
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-16 12:53:27 +0200

    [CHORE] Update SQL script

--- SQL Scripts/Explore.sql
+++ SQL Scripts/Explore.sql
@@ -1,3 +1,21 @@
 use librarydb;
 
 SELECT * from books;
+SELECT * from members;
+SELECT * from loans;
+
+-- delete loans table
+drop table loans;
+
+-- delete all rows from loans table
+delete from loans;
+
+create table loans (
+Id INT NOT NULL AUTO_INCREMENT,
+BookId int NOT null,
+MemberId int NOT null,
+LoanDate date NOT NULL,
+ReturnDate date,
+PRIMARY KEY (Id),
+FOREIGN KEY(BookId) references books(Id) on delete cascade,
+FOREIGN KEY(MemberId) references members(Id) on delete cascade);
