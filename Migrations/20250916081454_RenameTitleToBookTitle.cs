1481c576af2e78804bd25ce110c697de42373423
Divyendu Dutta <connect2divyendu@gmail.com>
2025-09-16 10:25:04 +0200

    [CHORE] Add the associated migrations for column rename
    Books.Title -> Books.BookTitle

---
+++ Migrations/20250916081454_RenameTitleToBookTitle.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibAdminSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTitleToBookTitle : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Books",
+                newName: "BookTitle");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BookTitle",
+                table: "Books",
+                newName: "Title");
+        }
+    }
+}
